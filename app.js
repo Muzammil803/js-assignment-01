c918c853d897546f76d34b8481cf529c58520b83
Muzammil <syeadmuzammil691@gmail.com>
2022-05-19 05:46:08 +0500

    work donr

---
+++ app.js
@@ -0,0 +1,48 @@
+
+// assignment 01 
+// question 01
+//alert()
+
+
+// question 02
+//var a = "Error ! Alert !"
+//alert(a)
+
+//question 03
+//var a ="I am Muzammil \n learning web and app devlopment from sylani "
+//alert(a) 
+
+//question 04
+//var a = "Error ! Alert !"
+//alert(a)
+
+//var b ="I am Muzammil \n learning web and app devlopment from sylani "
+//alert(b) 
+
+//question 05
+//var a = "I can run js through my web console "
+//console.log(a)
+
+
+
+//chap no #02
+
+// var name = "Muzammil"
+// alert(name
+// )
+// var age = 18
+// alert(age
+// )
+// var skill = "certified mobile app developer"
+// alert(skill
+// )
+// var a = "Muzammil \n muzam \n muz\n m "
+// alert(a
+// )
+// var email = "syedmuzammil691@gmail.com"
+// alert(email
+// )
+// var book = "smarter way to learn javascript"
+// alert(name
+// )
+
